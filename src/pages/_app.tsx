d2e73d645adf91ca07674becbf0d49ef29c4a5b9
Mateus Faria <mateusfariadomingos@hotmail.com>
2023-01-31 20:44:46 -0400

    feat: first settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Open_Sans } from '@next/font/google'
+
+const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-open-sans' })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${openSans.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
