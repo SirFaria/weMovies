d937b0f9c8b629362b5f56050fd965384ebe656b
Mateus Faria <mateusfariadomingos@hotmail.com>
2023-02-01 18:18:30 -0400

    feat: create cart context

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,73 @@
+import { createContext, ReactNode, useContext, useState } from 'react'
+
+interface ICartProviderProps {
+  children: ReactNode
+}
+
+export interface IMovies {
+  id: number
+  title: string
+  price: number
+  image: string
+}
+
+export interface ICartItems extends IMovies {
+  amount: number
+}
+
+interface ICartContext {
+  cartItems: ICartItems[]
+  addItemToCart: (movie: IMovies) => void
+  removeItemFromCart: (id: number) => void
+}
+
+export const CartContext = createContext<ICartContext>({} as ICartContext)
+
+export function CartProvider({ children }: ICartProviderProps) {
+  const [cartItems, setCartItems] = useState<ICartItems[]>([])
+
+  function addItemToCart({ id, title, price, image }: IMovies) {
+    // para impedir do item ser adicionado novamente ao carrinho
+    let exists = false
+    const newCart = cartItems.map(item => {
+      if (item.id === id) {
+        exists = true
+        return { ...item }
+      }
+      return item
+    })
+
+    if (exists) {
+      setCartItems(newCart)
+    } else {
+      setCartItems([
+        ...cartItems,
+        {
+          title,
+          price,
+          image,
+          id,
+          amount: 1,
+        },
+      ])
+    }
+  }
+
+  function removeItemFromCart(itemId: number) {
+    setCartItems(cartItems.filter(({ id }) => id !== itemId))
+  }
+
+  console.log(cartItems)
+
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addItemToCart, removeItemFromCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export function useCart() {
+  return useContext(CartContext)
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Header } from '~/components/Header'
+import { CartProvider } from '~/contexts/CartContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,10 +11,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>We Fit</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
-      <Header />
-      <main className='min-h-[calc(100vh-64px)] px-4 pb-4 flex flex-col gap-4 md:flex-row md:flex-wrap md:max-w-[960px] md:p-0 md:mx-auto md:content-start'>
-        <Component {...pageProps} />
-      </main>
+      <CartProvider>
+        <Header />
+        <main className='min-h-[calc(100vh-64px)] px-4 pb-4 flex flex-col gap-4 md:flex-row md:flex-wrap md:max-w-[960px] md:p-0 md:mx-auto md:content-start'>
+          <Component {...pageProps} />
+        </main>
+      </CartProvider>
     </>
   )
 }
