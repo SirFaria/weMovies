6122114aef4db1796102e8d65984da26d84be23c
Mateus Faria <mateusfariadomingos@hotmail.com>
2023-02-01 18:48:31 -0400

    feat: add data to shopping cart button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useCart } from '~/contexts/CartContext'
 
 export function Header() {
+  const { cartItems } = useCart()
+
   return (
     <header className='bg-bg-dark max-w-[960px] mx-auto flex justify-between py-[18px] px-[11px]'>
       <Link href='/'>
@@ -9,8 +12,12 @@ export function Header() {
       </Link>
       <div className='flex items-center'>
         <div className='flex flex-col items-end'>
-          {/* <h2 className='text-white font-semibold text-sm'>Meu Carrinho</h2> */}
-          <p className='text-text-gray font-semibold text-xs'>0 itens</p>
+          <h2 className='text-white font-semibold text-sm hidden invisible md:block md:visible'>
+            Meu Carrinho
+          </h2>
+          <p className='text-text-gray font-semibold text-xs'>
+            {cartItems.length} {cartItems.length === 1 ? 'item' : 'itens'}
+          </p>
         </div>
         <Link href='/cart'>
           <Image
