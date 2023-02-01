73d789e4487c4017de494ccd722310785fe73755
Mateus Faria <mateusfariadomingos@hotmail.com>
2023-02-01 18:27:53 -0400

    feat: add data to cart and cart itens

--- src/components/CartItemDesktop.tsx
+++ src/components/CartItemDesktop.tsx
@@ -1,23 +1,36 @@
 import Image from 'next/image'
+import { ICartItems, useCart } from '~/contexts/CartContext'
 import { Counter } from './Counter'
 
-export function CartItemDesktop() {
+interface ICartItemDesktopProps {
+  movieData: ICartItems
+}
+
+export function CartItemDesktop({ movieData }: ICartItemDesktopProps) {
+  const { removeItemFromCart } = useCart()
+  const subtotal = movieData.amount * movieData.price
+
   return (
     <tr className='w-full'>
       <td>
         <div className='flex items-center'>
           <Image
-            width={147}
-            height={188}
+            width={89}
+            height={114}
             className='w-fit '
             alt='Imagem da capa do filme'
-            src='/assets/viuva-negra.png'
+            src={movieData.image}
           />
           <div className='flex flex-col pl-[52px]'>
             <p className='text-bg-dark text-sm font-bold whitespace-nowrap'>
-              Homem Aranha
+              {movieData.title}
             </p>
-            <strong className='text-bg-dark whitespace-nowrap'>R$ 29,99</strong>
+            <strong className='text-bg-dark whitespace-nowrap'>
+              {new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(movieData.price)}
+            </strong>
           </div>
         </div>
       </td>
@@ -27,17 +40,24 @@ export function CartItemDesktop() {
       </td>
 
       <td>
-        <strong className='text-bg-dark whitespace-nowrap'>R$ 29,99</strong>
+        <strong className='text-bg-dark whitespace-nowrap'>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(subtotal)}
+        </strong>
       </td>
 
       <td>
-        <Image
-          width={16}
-          height={18}
-          className='ml-auto'
-          alt='Ícone de lixeira'
-          src='/assets/trash-can.svg'
-        />
+        <button onClick={() => removeItemFromCart(movieData.id)}>
+          <Image
+            width={16}
+            height={18}
+            className='ml-auto'
+            alt='Ícone de lixeira'
+            src='/assets/trash-can.svg'
+          />
+        </button>
       </td>
     </tr>
   )
--- src/components/CartItemMobile.tsx
+++ src/components/CartItemMobile.tsx
@@ -1,23 +1,34 @@
 import Image from 'next/image'
+import { ICartItems } from '~/contexts/CartContext'
 import { Counter } from './Counter'
 
-export function CartItemMobile() {
+interface ICartItemMobileProps {
+  movieData: ICartItems
+}
+
+export function CartItemMobile({ movieData }: ICartItemMobileProps) {
+  const subtotal = movieData.amount * movieData.price
   return (
     <div className='w-full flex gap-4 justify-between md:invisible md:hidden'>
       <Image
-        width={147}
-        height={188}
-        className='w-fit '
+        width={64}
+        height={82}
+        className='w-fit max-w-[64px]'
         alt='Imagem da capa do filme'
-        src='/assets/viuva-negra.png'
+        src={movieData.image}
       />
 
       <div className='flex flex-col gap-4 flex-1'>
         <div className='flex gap-4 items-center'>
           <p className='text-bg-dark text-sm font-bold whitespace-nowrap'>
-            Homem Aranha
+            {movieData.title}
           </p>
-          <strong className='text-bg-dark whitespace-nowrap'>R$ 29,99</strong>
+          <strong className='text-bg-dark whitespace-nowrap ml-auto'>
+            {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            }).format(movieData.price)}
+          </strong>
           <Image
             width={16}
             height={18}
@@ -32,7 +43,12 @@ export function CartItemMobile() {
             <span className='text-text-gray text-xs font-bold uppercase'>
               Subtotal
             </span>
-            <strong className='text-bg-dark whitespace-nowrap'>R$ 29,99</strong>
+            <strong className='text-bg-dark whitespace-nowrap'>
+              {new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(subtotal)}
+            </strong>
           </div>
         </div>
       </div>
