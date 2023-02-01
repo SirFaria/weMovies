d3670fc4552835c8a2c0a82922fbe0ea4e064199
Mateus Faria <mateusfariadomingos@hotmail.com>
2023-02-01 01:01:41 -0400

    refactor: move meta tag and change page language

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang='pt-BR'>
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
