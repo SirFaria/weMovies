8bebda8be6a3ab4ea19af75da56abe7e4c7f1c67
Mateus Faria <mateusfariadomingos@hotmail.com>
2023-02-01 18:17:31 -0400

    feat: add navigation between home and cart page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,26 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <header className='bg-bg-dark max-w-[960px] mx-auto flex justify-between py-[18px] px-[11px]'>
-      <h1 className='text-white font-bold size text-xl'>WeMovies</h1>
+      <Link href='/'>
+        <h1 className='text-white font-bold size text-xl'>WeMovies</h1>
+      </Link>
       <div className='flex items-center'>
         <div className='flex flex-col items-end'>
           {/* <h2 className='text-white font-semibold text-sm'>Meu Carrinho</h2> */}
           <p className='text-text-gray font-semibold text-xs'>0 itens</p>
         </div>
-        <Image
-          width={30}
-          height={25}
-          className='ml-[9px]'
-          src='/assets/shopping-bag.svg'
-          alt='ícone de bolsa de shopping'
-        />
+        <Link href='/cart'>
+          <Image
+            width={30}
+            height={25}
+            className='ml-[9px]'
+            src='/assets/shopping-bag.svg'
+            alt='ícone de bolsa de shopping'
+          />
+        </Link>
       </div>
     </header>
   )
